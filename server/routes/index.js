c1f501e3ebc6b15bd691da2bc14388ecb6a01f4d
Lin Chun <yichun03@gmail.com>
2020-05-06 15:35:04 +0800

    add route plus

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const selfName = process.env.name;
+router.get('/plus/a/:a/b/:b', (req, res) => {
+  return res.status(200).json({ message: `a + b = ${parseFloat(req.params.a) + parseFloat(req.params.b)}`, from: selfName});
 });
+
 
 module.exports = router;
