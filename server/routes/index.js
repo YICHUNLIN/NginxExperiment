0dae77ff02af57a6ae1de7336f707085357e953e
Lin Chun <yichun03@gmail.com>
2020-05-06 17:40:26 +0800

    configure cal

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 const selfName = process.env.name;
+//var sleep = require('sleep');
 router.get('/plus/a/:a/b/:b', (req, res) => {
-  return res.status(200).json({ message: `a + b = ${parseFloat(req.params.a) + parseFloat(req.params.b)}`, from: selfName});
+  const result = parseFloat(req.params.a) + parseFloat(req.params.b);
+  return res.status(200).json({ message: `a + b = ${result}`, from: selfName});
 });
 
 
